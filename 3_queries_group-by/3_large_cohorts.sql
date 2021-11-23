24871a5643d3361e66fd14c7ea5e7603236eb0ff
github4maninder <maninder.email@gmail.com>
2021-11-23 14:24:01 -0500

    implement largeCohort query

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as cohort_name, count(students.*) AS student_count 
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+GROUP BY cohort_name 
+HAVING count(students.*) >= 18
+ORDER BY student_count;
