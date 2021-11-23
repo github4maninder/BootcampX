9f9a090b9d923e15a203719f9ce439bee657d14c
github4maninder <maninder.email@gmail.com>
2021-11-23 14:34:59 -0500

    implement totalAssignmentSubmissions

---
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
