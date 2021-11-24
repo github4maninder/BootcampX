100d529b5df122f9f47507c387ede019a5dfd735
github4maninder <maninder.email@gmail.com>
2021-11-24 00:13:05 -0500

    implement Total Assignments and duration query

---
+++ 4_queries/12_name_of_teachers_that_assisted.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;
