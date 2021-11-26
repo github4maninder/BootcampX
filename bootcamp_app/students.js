bdc0cbec1935caa81f7297afcee859defea202f9
github4maninder <maninder.email@gmail.com>
2021-11-25 22:45:10 -0500

    Update students.js and teachers.js to use parameterized queries.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,33 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+// const args = process.argv.slice(2);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+values = [`%${cohortName}%`, limit]
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  });
 
+
+
+// pool.query(`
+// SELECT students.id, students.name, cohorts.name
+// FROM students
+// JOIN cohorts ON cohort_id = cohorts.id
+// LIMIT 5;
+// `)
+// .then(res => {
+//   console.log(res.rows);
+// })
+// .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,34 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+values = [`%${cohortName}%`];
+
 pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
+JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;
+`, values)
+  .then(res => {
+    //loop through each row!
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    })
+  });
+  
+// pool.query (`
+//   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+//   FROM students
+//   JOIN cohorts ON cohorts.id = cohort_id
+//   WHERE cohorts.name LIKE $1
+//   LIMIT $2;
+//   `;
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
